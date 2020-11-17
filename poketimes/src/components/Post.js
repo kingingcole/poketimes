0312e240941b25e9620e2fc014c6f1c548f15ff5
Emeruche Cole <ColeRuche@admins-MacBook-Air.local>
2020-11-17 19:49:09 +0100

    converted post.js to function component

--- poketimes/src/components/Post.js
+++ poketimes/src/components/Post.js
@@ -1,34 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import axios from 'axios'
 
-class Post extends Component{
-    state = {
-        post: null
-    }
-    componentDidMount(){
-        let id = this.props.match.params.post_id;
+const Post = (props) => {
+    const [post, setPost] = useState(null)
+
+    useEffect(() => {
+        let id = props.match.params.post_id;
         axios.get('https://jsonplaceholder.typicode.com/posts/' + id)
         .then(res => {
-            this.setState({
-                post: res.data
-            })
+            setPost(res.data)
         })
-        
-    }
-    render() {
-        const post = this.state.post ? (
-            <div className="post">
-                <h4 className="center">{this.state.post.title}</h4>
-                <p>{this.state.post.body}</p>
-            </div>
-        ) : (
-            <div className="center">Loading post...</div>
-        )
-        return (
-            <div className="container">
-                { post }
-            </div>
-        )
-    }
+    }, [])
+
+    const postToRender = post ? (
+        <div className="post">
+            <h4 className="center">{post.title}</h4>
+            <p>{post.body}</p>
+        </div>
+    ) : (
+        <div className="center">Loading post...</div>
+    )
+
+    return (
+        <div className="container">
+            { postToRender }
+        </div>
+    ) 
 }
+
 export default Post
